b0ae0254f56a1224076dafc2e3169c791b6c5c0f
EtiennePelletier <etpelletier93@hotmail.com>
2016-09-29 18:27:15 -0400

    Add advanced user features
      - Add 'Change password' and 'Create account' features,
      - Move 'credentials' collection to from 'users' to 'biobot' database,
      - Create an 'admin-only' flask wrapper,
      - Add admin-only page 'Manage users', allowing to view users, delete them
        and change their admin status,
      - Add autofocus to input fields for all login-related pages,
      - Update documentation.

--- README.md
+++ README.md
@@ -1,10 +1,8 @@
 # BioBot Web Application
 
-The web page is built with a combination of popular Open Source tools : [Python Flask], [Bootstrap] and [DataTables]. It is planned to include [MongoDB] in the short term future.
+The web page is built with a combination of popular Open Source tools : [Python Flask], [Roslibjs], [Bootstrap], [DataTables], [MongoDB] and [MJPG-Streamer].
 
 ![BioBot Home Page](/static/img/home_page.png "BioBot Home Page")
-
-The webcam showing real-time video of the robot in tab `Surveillance` is using [MJPG-Streamer].
 
 It should run from the Jetson TK1 device. To start: `$ ./start_web.sh`. Then, from any browser on the network, navigate to URL `<Jetson TK1's IP address>:5000`. Here are the complete usage options:
 
@@ -25,9 +23,22 @@ optional arguments:
   -D, --debug           Start Flask app in debug mode. Default: False
 ```
 
+## Quick User Guide
+| Page | Description |
+| ---- | ----------- |
+| Login | Log in to the web application. Creating account and changing password features are implemented. |
+| Surveillance | Display live stream video of the robot, which comes from the webcam. |
+| Manual Control* | Manually control BioBot : Axis, Single Pipette, Multiple Pipette and Gripper. The current position of the robot refreshes automatically after every step. |
+| Biological Protocol* | Create, modify, open or save deck and protocol files, as well as sending them to the Planner ROS node.
+| Manage Users** | View all users, last login time and admin status. Can change admin status of users and delete them. |
+&ast; Requires login  
+&ast;&ast;Requires administrator rights
+
 [Python Flask]: <http://flask.pocoo.org/>
+[Roslibjs]: <http://wiki.ros.org/roslibjs>
 [Bootstrap]: <http://getbootstrap.com/>
 [DataTables]: <https://datatables.net/>
 [MongoDB]: <https://www.mongodb.com/>
 [MJPG-Streamer]: <https://sourceforge.net/projects/mjpg-streamer/>
+
 
--- biobot_web.py
+++ biobot_web.py
@@ -2,12 +2,15 @@
 # -*- coding: utf-8 -*-
 
 import argparse
+import datetime
 from flask import Flask, Markup, Response, abort, flash, redirect, \
                   render_template, request, url_for
-from flask_login import LoginManager, UserMixin, login_required, login_user, \
-                        logout_user
+from flask_login import LoginManager, UserMixin, current_user, login_required, \
+                        login_user, logout_user
+from functools import wraps
 import json
 import hashlib
+import pandas
 import pymongo
 import time
 import uuid
@@ -22,9 +25,13 @@ def check_password(hashed_password, user_password):
     password, salt = hashed_password.split(':')
     return password == hashlib.sha256(salt.encode() + user_password.encode()).hexdigest()
 
-def convert_ts(ts):
-    """Convert timestamp to human-readable string"""
-    return datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
+def admin_required(func):
+    @wraps(func)
+    def decorated_function(*args, **kwargs):
+        if not current_user.is_admin():
+            return redirect(url_for('bad_permissions'))
+        return func(*args, **kwargs)
+    return decorated_function
 
 # Load default configuration
 with open('config.json') as config:
@@ -56,7 +63,7 @@ conf.__dict__.update(args.__dict__)
 
 # Get MongoDB Database Client
 client = pymongo.MongoClient("{0}:{1}".format(conf.db_host, conf.db_port))
-users = client['users']
+biobot = client['biobot']
 
 # Create Flask Application
 app = Flask(__name__)
@@ -68,7 +75,7 @@ login_manager.init_app(app)
 class User(UserMixin):
     def __init__(self, username):
         self.username = username
-        user = users.credentials.find_one({'username': username})
+        user = biobot.credentials.find_one({'username': username})
         self.admin = user['admin']
 
     def get_id(self):
@@ -97,9 +104,11 @@ def login():
         next = request.args.get('next')
         username = request.form['username']
         password = request.form['password']
-        user = users.credentials.find_one({'username': username})
+        user = biobot.credentials.find_one({'username': username})
         if user and check_password(user['password'], password):
             login_user(User(username))
+            biobot.credentials.update_one(user, {'$set':
+                                          {'last_login' : time.time()}})
             return redirect(next or url_for('home'))
         else:
             flash('Invalid credentials', 'danger')
@@ -113,6 +122,65 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('home'))
+
+@app.route('/create_account', methods=['GET', 'POST'])
+def create_account():
+    if request.method == 'POST':
+        next = request.args.get('next')
+        username = request.form['username']
+        password = request.form['password']
+        password_confirm = request.form['password_confirm']
+
+        if not password:
+            flash('Password cannot be empty', 'danger')
+            return render_template('create_account.html')
+
+        if password != password_confirm:
+            flash('Both password entries do not match', 'danger')
+            return render_template('create_account.html')
+
+        if not username.replace('_', '').isalnum():
+            flash('Invalid username (letters, numbers and underscores only)', 'danger')
+            return render_template('create_account.html')
+
+        user = biobot.credentials.find_one({'username': username})
+        if user or not username:
+            flash('Username not available', 'danger')
+            return render_template('create_account.html')
+
+        biobot.credentials.insert_one({'username': username,
+                                     'password': hash_password(password),
+                                     'admin': False})
+        flash('Account created successfully', 'success')
+        return redirect(url_for('login'))
+
+    else:
+        return render_template('create_account.html')
+
+@app.route('/change_password', methods=['GET', 'POST'])
+def change_password():
+    if request.method == 'POST':
+        username = request.form['username']
+        old_password = request.form['old_password']
+        new_password = request.form['new_password']
+
+        user = biobot.credentials.find_one({'username': username})
+        if user and check_password(user['password'], old_password):
+            if not new_password:
+                flash('Password cannot be empty', 'danger')
+                return render_template('change_password.html')
+
+            biobot.credentials.update_one(user, {'$set': {
+                                          'password': hash_password(new_password)}})
+            flash('Password changed successfully', 'success')
+            return redirect(url_for('login'))
+
+        else:
+            flash('Invalid credentials', 'danger')
+            return render_template('change_password.html')
+
+    else:
+        return render_template('change_password.html')
 
 @app.route('/home')
 def home():
@@ -132,12 +200,54 @@ def control():
 def protocol():
     return render_template('protocol.html')
 
+@app.route('/manage_users')
+@login_required
+@admin_required
+def manage_users():
+    user_list = list(biobot.credentials.find())
+    return render_template('manage_users.html', users=user_list)
+
+@app.route('/manage_users/demote/<username>')
+@login_required
+@admin_required
+def demote_user(username):
+    biobot.credentials.update_one({'username': username}, {'$set': {'admin': False}})
+    flash("User {0} reverted to standard user successfully".format(username), 'info')
+    return redirect(url_for('manage_users'))
+
+@app.route('/manage_users/promote/<username>')
+@login_required
+@admin_required
+def promote_user(username):
+    biobot.credentials.update_one({'username': username}, {'$set': {'admin': True}})
+    flash("User {0} promoted to administrator successfully".format(username), 'info')
+    return redirect(url_for('manage_users'))
+
+@app.route('/manage_users/delete/<username>')
+@login_required
+@admin_required
+def delete_user(username):
+    if current_user.get_id() == username:
+        flash('Cannot delete yourself', 'danger')
+    else:
+        biobot.credentials.delete_one({'username': username})
+        flash("User {0} deleted successfully".format(username), 'info')
+    return redirect(url_for('manage_users'))
+
+@app.route('/bad_permissions')
+def bad_permissions():
+    return render_template('bad_permissions.html')
+
 @app.errorhandler(404)
 def page_not_found(error):
     """This method handles all unexisting route requests"""
     return render_template('404.html'), 404
 
 # Add methods that can be called from the Jinja2 HTML templates
+def convert_ts(ts):
+    """Convert timestamp to human-readable string"""
+    return datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
+
 def format_sidebar(name, icon, url):
     """
     Used to generate HTML line for sidebar in layout.html.
@@ -151,7 +261,7 @@ def format_sidebar(name, icon, url):
 
     return Markup(html)
 
-app.jinja_env.globals.update(format_sidebar=format_sidebar)
+app.jinja_env.globals.update(format_sidebar=format_sidebar, convert_ts=convert_ts)
 
 # Start the application
 if __name__ == '__main__':
