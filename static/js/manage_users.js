1ae349bf5a68d0e9c80723d207b6275bea2c9c2d
EtiennePelletier <etpelletier93@hotmail.com>
2016-10-28 22:57:46 -0400

    Add alerts to the Manage Users page
     - Replace table hyperlinks to promote/demote/delete users by buttons that
       open a popup alert, who send the redirect requests on admin confirmation,
     - Replace <th>s for <td>s in table body.

---
+++ static/js/manage_users.js
@@ -0,0 +1,38 @@
+function promote_user(user) {
+    BootstrapDialog.confirm({
+        title: 'Promote user?',
+        message: 'Are you sure you wish to promote ' + user + ' to administrator?',
+        type: BootstrapDialog.TYPE_SUCCESS,
+        btnOKLabel: 'Confirm',
+        callback: function(result){
+            if(result)
+                window.location.assign(window.location.href + '/promote/' + user);
+        }
+    });
+}
+
+function revert_user(user) {
+    BootstrapDialog.confirm({
+        title: 'Revert user?',
+        message: 'Are you sure you wish to revert ' + user + ' to standard user?',
+        type: BootstrapDialog.TYPE_WARNING,
+        btnOKLabel: 'Confirm',
+        callback: function(result){
+            if(result)
+                window.location.assign(window.location.href + '/demote/' + user);
+        }
+    });
+}
+
+function delete_user(user) {
+    BootstrapDialog.confirm({
+        title: 'Delete user?',
+        message: 'Are you sure you wish to delete user ' + user + '?',
+        type: BootstrapDialog.TYPE_DANGER,
+        btnOKLabel: 'Confirm',
+        callback: function(result){
+            if(result)
+                window.location.assign(window.location.href + '/delete/' + user);
+        }
+    });
+}
