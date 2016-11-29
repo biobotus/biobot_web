a99973dee7eb65930dd3894e1660a870b03cd5b0
EtiennePelletier <etpelletier93@hotmail.com>
2016-11-29 16:41:56 -0500

    Improve display of colony characteristics

--- biobot_web.py
+++ biobot_web.py
@@ -56,11 +56,11 @@ def closest_color(requested_color):
     min_colors = {}
     for key, name in webcolors.css21_hex_to_names.items():
         r_c, g_c, b_c = webcolors.hex_to_rgb(key)
-        rd = (r_c - requested_colour[0]) ** 2
-        gd = (g_c - requested_colour[1]) ** 2
-        bd = (b_c - requested_colour[2]) ** 2
-        min_colours[(rd + gd + bd)] = name
-    return min_colours[min(min_colours.keys())]
+        rd = (r_c - requested_color[0]) ** 2
+        gd = (g_c - requested_color[1]) ** 2
+        bd = (b_c - requested_color[2]) ** 2
+        min_colors[(rd + gd + bd)] = name
+    return min_colors[min(min_colors.keys())]
 
 def get_color_name(requested_color):
     try:
